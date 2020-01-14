3aa9cbf40ba8630b5d864868d24b638f54e9a78a
snake <tiramsnake@gmail.com>
2020-01-14 22:10:12 +0200

    Lines

--- GUI/ProjectManager.cpp
+++ GUI/ProjectManager.cpp
@@ -1,6 +1,7 @@
 #include "ProjectManager.hpp"
 #include "TetrisPG.hpp"
 #include "CheckpointPG.hpp"
+#include "LinesPG.hpp"
 #include "projects/NeatProject.hpp"
 #include <filesystem>
 #include <boost/property_tree/ptree.hpp>
@@ -119,6 +120,10 @@ neuroevolution::IPlayground& ProjectManager::createPlayground(const std::string&
    {
       mPlayground = std::make_unique<CheckpointPG>();
    }
+   else if(name == "Lines")
+   {
+      mPlayground = std::make_unique<LinesPG>();
+   }
    else
    {
       throw -1;
@@ -131,7 +136,8 @@ std::vector<std::string> ProjectManager::getPlaygroundList() const
 {
    return {
       "Tetris",
-      "Checkpoint"
+      "Checkpoint",
+      "Lines"
    };
 }
 
